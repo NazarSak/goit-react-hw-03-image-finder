f57cedad107eaa944f52ce121310ad52978f55d1
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-03-19 15:38:08 +0200

    1234

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Notiflix from 'notiflix';
 import Searchbar from './searchbar/Searchbar';
-import { ContentInfo } from './contentInfo/ContentInfo';
 import { getNews } from 'services/getNews';
 import { Button } from './button/Button';
-import { isVisible } from '@testing-library/user-event/dist/utils';
+import { Loader } from 'components/loader/Loader';
+import { ImageGallery } from 'components/imageGallery/ImageGallery';
 
 export class App extends Component {
   state = {
@@ -11,71 +12,49 @@ export class App extends Component {
     page: 1,
     arrayImg: [],
     isLoading: false,
-    isVisible: false,
+    buttonTogle: false,
+    data: null,
   };
 
   onLoadMore = () => {
     this.setState(prevState => ({
       page: prevState.page + 1,
     }));
-    this.setState({
-      isLoading: true,
-    });
   };
 
-  async componentDidUpdate(_, prevState) {
-    const prevQuery = prevState.searchText;
-    const currentQuery = this.state.searchText;
-
-    if (currentQuery !== prevQuery || this.state.page !== prevState.page) {
-      try {
-        this.setState({ isLoading: true });
-        const response = await getNews.fetchImages(
-          currentQuery,
-          this.setState.page
-        );
-        console.log(response);
-
-        if (response.totalHits < 12) {
-          this.setState({ isVisible: false });
-        } else {
-          this.setState({ isVisible: true });
-        }
-
-        this.setState({ isVisible: false });
-        this.setState(prev => ({
-          arrayImg: [...prev.arrayImg, ...response.hits],
-        }));
-      } catch (error) {
-        console.log(error);
-      }
+  buttonTogle = length => {
+    if (length >= 12) {
+      return this.setState({ buttonTogle: true });
     }
+    return this.setState({ buttonTogle: false });
+  };
 
-    // if (prevState.page < this.state.page) {
-    //   getNews(this.props.searchText,this.state.page)
-    //   .then((response) => {response.json()})
-    //   .then((data) => {
-    //     this.setState({
-    //       arrayImg: [data.hits],
-    //     }));
-    //   });
-    // }
+  componentDidUpdate(_, prevState) {
+    if (prevState.searchText !== this.state.searchText) {
+      this.setState({ isLoading: true });
+      getNews(this.state.searchText, this.state.page)
+        .then(response => response.json())
+        .then(data => {
+          if (data.hits.length === 0) {
+            Notiflix.Notify.failure(
+              'Sorry, but nothing was found for your search'
+            );
+          }
+          const hits = data.hits;
+          this.buttonTogle(hits.length);
+
+          this.setState({ data: data.hits });
+        })
+        .catch(error => {
+          console.log(error);
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
+        });
+    }
   }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevProps.searchText !== this.props.searchText) {
-  //     getNews(this.props.searchText)
-  //       .then(response => response.json())
-  //       .then(data => {
 
-  //         this.setState({ arrayImg: data.hits, });
-  //       })
-  //       .catch(error => {
-  //         console.log(error);
-
-  //       });
-  //   }
-  // }
 
   handleSearch = searchText => {
     this.setState({ searchText });
@@ -83,12 +62,13 @@ export class App extends Component {
 
   render() {
     const { handleSearch } = this;
-    const { searchText } = this.state;
+    const { data, isLoading, buttonTogle } = this.state;
     return (
       <>
         <Searchbar handleSearch={handleSearch} />
-        <ContentInfo searchText={searchText} />
-        {isVisible && <Button onClick={this.onLoadMore} />}
+        {isLoading && <Loader />}
+        {data && <ImageGallery data={data} />}
+        {buttonTogle && <Button onClick={this.onLoadMore} />}
       </>
     );
   }
